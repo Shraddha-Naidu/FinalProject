e89c87e53b0f49c02de2aab3b58b7d7a8f171a08
Emily Bozek <emily.bozek@gmail.com>
2022-05-14 11:25:30 -0700

    login/logout establish

--- src/routes.js
+++ src/routes.js
@@ -14,35 +14,7 @@ route.get('/', (req, res) => {
 // Login/Logout Routes
 // authenticates user
 route.post('/', db.loginQuery)
-// verifies that the user is logged in
-route.get('/dashboard', db.verifyLogin)
 //logout
 route.get('/logout', db.destroySession)
-
-
-// Client Routes
-route.get('/clients/socialworker/:socialworkerid', db.getClientsBySocialWorker)
-route.get('/clients/:id', db.getClientById)
-route.post('clients', db.createClient)
-route.put('/clients/:id', db.updateClient)
-
-// Resources Routes
-route.get('/resources', db.getResources)
-route.get('/resources/:id', db.getResourcesById)
-
-// Flags Routes
-route.get('/flags/clients/:clientid', db.getFlagsByClientId)
-route.post('flags', db.createFlag)
-route.delete('/flags/:id', db.deleteFlag)
-
-// To-Dos Routes
-route.get('/to-dos/:day', db.getToDosByDay)
-route.post('to-dos', db.createToDo)
-route.delete('to-dos/:id', db.deleteToDo)
-
-// Client File Comments Routes
-route.get('/comments/clients/:clientid', db.getCommentsByClientId)
-route.post('comments', db.createComment)
-route.delete('comments/:id', db.deleteComment)
 
 module.exports = route;
