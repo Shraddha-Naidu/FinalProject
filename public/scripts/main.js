85c3cca8ecb1551857988285931135e344e756fd
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-15 10:46:02 -0600

    Add Side Bar

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,9 @@
 console.log('Hello!');
+
+//// COLLAPSES SIDEBAR ////
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+document.querySelector('button').onclick = function () {
+  sidebar.classList.toggle('sidebar_small');
+  mainContent.classList.toggle('main-content_large')
+}
