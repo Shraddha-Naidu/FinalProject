35ed0f16fee0b344e2f4ad2810adb6991237b008
Emily Bozek <emily.bozek@gmail.com>
2022-05-14 20:26:35 -0700

    make basic controllers and partial views for widgets and pages in app - clientFile, intakeForm, calendar widget, clientList widget, map widget, toDos widget

--- src/controllers/clientFile.js
+++ src/controllers/clientFile.js
@@ -3,6 +3,11 @@ const route = express.Router();
 // this connects the database connection and queries to the routing folder
 const db = require('../models/db')
 
+// db.getClientById, db.getResources, db.getFlagsbyClientId, db.getCommentsByClientId
+
+route.get('/', (req, res) => {
+  res.render('clientFile');
+});
 
 
 
--- src/controllers/intakeForm.js
+++ src/controllers/intakeForm.js
@@ -3,5 +3,8 @@ const route = express.Router();
 // this connects the database connection and queries to the routing folder
 const db = require('../models/db')
 
+route.get('/', (req, res) => {
+   res.render('intakeForm');
+});
 
 module.exports = route;
