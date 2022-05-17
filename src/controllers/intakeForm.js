5b82ef2e955099190d9951b7e070914921f69937
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-17 10:57:18 -0600

    Trying to find route bug

--- src/controllers/intakeForm.js
+++ src/controllers/intakeForm.js
@@ -5,6 +5,7 @@ const route = express.Router();
 module.exports = (db) => {
    // Intake Form
    route.get('/', (req, res) => {
+      console.log('Heading to intake form...')
       res.render("intakeForm")
    });
    return route;
