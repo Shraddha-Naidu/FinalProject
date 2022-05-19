63671011d17f9eed7b319e89065fdb272905ff97
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-19 12:15:43 -0600

    Pseudocode Edit

--- src/controllers/intakeForm.js
+++ src/controllers/intakeForm.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const route = express.Router();
 
-//Create route for new client to blank form
-//Create route to existing client filled form that can be edited and updated in db
-//On intake user should be asked whether its a new client or existing
+//First page leeads to new client/returning client as
+//If new client then to blank form
+//Else to client Directory to find returning client(inActive) and click on name to generate dynamically filled form
+//
+//CREATE for new client
+//EDIT Create route to existing client filled form that can be edited and updated in db
+//DELETE to turn inActive
 
 
 module.exports = (db) => {
