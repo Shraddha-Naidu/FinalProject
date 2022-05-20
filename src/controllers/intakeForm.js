0e1e691cab58b73be1110f85c3e9932a8afba293
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-20 09:20:00 -0600

    Add intake Form route

--- src/controllers/intakeForm.js
+++ src/controllers/intakeForm.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const route = express.Router();
 
+const { addNewClient } = require('lib/helperFunctions.js')(db);
+
 //First page leeads to new client/returning client as
 //If new client then to blank form
 //Else to client Directory to find returning client(inActive) and click on name to generate dynamically filled form
@@ -11,9 +13,21 @@ const route = express.Router();
 
 
 module.exports = (db) => {
-   // Intake Form
+   // Blank Intake Form
    route.get('/', (req, res) => {
       res.render("intakeForm")
    });
+
+   //Filled Intake Form to send Data
+  route.post('/', (req, res) => {
+      addNewClient(req.body.name, req.body.age, req.body.email, req.body.phone, req.body.isHoused, req.body.address, req.body.known_locations, req.body.dependants, req.body.dependents_list, req.body.citizenship, req.body.applied_at)
+      .then ((clientData) => {
+         res.send(clientData)
+      })
+      .catch((err) => {
+         console.log(err.message)
+       })
+ 
+   });
    return route;
-   };
+ };
