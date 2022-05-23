6d1405f36640401e3d5b91e1b64651c984c4aa7f
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-23 07:58:02 -0600

    Add updated query to send data to seperate tables

--- src/controllers/intakeForm.js
+++ src/controllers/intakeForm.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const route = express.Router();
 
-const addNewClient = 'INSERT INTO clients (user_id, name, age, email, phone, isHoused, address, known_locations, dependants, dependents_list, citizenship, applied_at)'
+const addNewClient = 'INSERT INTO clients (user_id, name, age, email, phone, isHoused, address, known_locations, dependants, dependents_list, citizenship, applied_at) VALUES ($1,$2,$3,$4,$5,$6,$8,$9,$10,$11) RETURNING *;'
    
 
 module.exports = (db) => {
@@ -12,7 +12,8 @@ module.exports = (db) => {
 
    //Filled Intake Form to send Data
   route.post('/', (req, res) => {
-      db.query(addNewClient)
+     const user_id = req.body.id
+      db.query(addNewClient, [user_id, name, age, email, phone, isHoused, address, known_locations, dependants, dependents_list, citizenship, applied_at])
       .then ((clientData) => {
          res.send(clientData)
       })
@@ -22,3 +23,20 @@ module.exports = (db) => {
    });
    return route;
  };
+
+
+ //QUERY CHANGE TO ADD RESOURCES//
+/*
+WITH ins AS (
+  INSERT INTO clients
+  (user_id, name, age, email, phone, isHoused, address, known_locations, dependants, dependents_list, citizenship, applied_at)
+VALUES
+  ($1,$2,$3,$4,$5,$6,$8,$9,$10,$11)
+RETURNING id),
+ins2 AS (
+INSERT INTO resources
+  (client_id, resources_requested)
+VALUES
+  ($1,$2)
+)
+ */
