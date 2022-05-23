900aa5cb90d580ef9462c98fa462aea9223d9cbc
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-23 12:31:01 -0600

    Submit Update

--- public/scripts/newClientIntake.js
+++ public/scripts/newClientIntake.js
@@ -8,3 +8,38 @@
 
 /// DOM ///
 
+/* $(document).ready(function () {
+//on Click Add New Client
+$("#intake-form").on('submit', function (event) {
+  alert("New Client Added")
+  event.preventDefault();//prevents default submission behaviour
+
+  var $form = $(this),
+  url = $form.attr('action');
+
+  var sendData = $.post(url, {
+    name: $('#name').val(),
+    age: $('#age').val(),
+    phone: $('#phone').val(),
+    email: $('#email').val(),
+    housed: $('#housed').val(),
+    address: $('#address').val(),
+    locations: $('#locations').val(),
+    citizenship: $('#citizenship').val(),
+    dependents: $('#dependents').val(),
+    dependentList: $('#dependentList').val(),
+    reosurces: [
+      counselling,
+      housing,
+      Legal,
+      addictions,
+      child_care,
+      shelter,
+      career
+    ],
+    known_locations: $('#known_locations'),
+    applied_date: ('#applie_date')
+  })
+  })
+});
+ */
