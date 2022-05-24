8d07ddb69e85acd66d537365f1370663a4375c9d
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-24 15:48:28 -0600

    Add file for new Resource script

---
+++ public/scripts/addNewResource.js
@@ -0,0 +1,20 @@
+// FUNCTIONS //
+
+// Create New Resource Element//
+const newResource = function(resource) {
+
+}
+
+//Pulls and renders all resources
+const renderResources = function(allResources) {
+
+}
+
+//Loads resources
+const loadResources = function() {
+
+}
+
+
+//DOM//
+
