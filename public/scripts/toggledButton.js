f8766654bc1af3020efce86505bdaeb97678f790
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-25 22:48:03 -0600

    Finalize Intake form

--- public/scripts/toggledButton.js
+++ public/scripts/toggledButton.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
     const id = window.location.pathname.substr(12, pathname.length)
     if ($(this).hasClass('btn-danger')) {
       $.post("/clientfile/status", { status: true, id }, function (data) {
-        $('.toggle-button').removeClass('btn-danger').addClass('btn-success').text('In-Process');
+        $('.toggle-button').removeClass('btn-danger').addClass('btn-success').text('ACTIVE');
         $(this).addClass('btn-success').removeClass('btn-danger');
       })
     }
 
     else if ($(this).hasClass('btn-success')) {
       $.post("/clientfile/status", { status: false, id }, function (data) {
-        $('.toggle-button').removeClass('btn-success').addClass('btn-danger').text('Inactive');
+        $('.toggle-button').removeClass('btn-success').addClass('btn-danger').text('INACTIVE');
         $(this).addClass('btn-danger').removeClass('btn-success');
       })
     }
