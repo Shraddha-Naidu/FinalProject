01beff774833b17bbab3f64cb11517943d8ce624
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-26 11:34:08 -0600

    Add post route for new resource

--- src/controllers/resources.js
+++ src/controllers/resources.js
@@ -13,7 +13,22 @@ module.exports = (db) => {
        .catch((e) => {
                console.error(e);
                res.send(e);
-             })
+       })
    });
+
+  route.post('/resources', (req, res) => {
+    const { provider, resource_type, provider_phone, provider_email, provider_address } = req.body
+    addNewResource = `INSERT INTO clients (provider, resource_type, provider_phone, provider_email, provider_address) VALUES ($1, $2, $3, $4, $5) RETURNING *;`
+
+    db.query(addNewResource, [provider, resource_type, provider_phone, provider_email, provider_address])
+    .then((data) => {
+      res.status(201).send();
+      console.log(data);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.send(e);
+    });
+  })
    return route;
    };
