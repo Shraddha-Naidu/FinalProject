97bf354602f5f88250508f6cf1daca503d32c34a
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-26 06:19:59 -0600

    Fix Modal

--- public/scripts/addNewResource.js
+++ public/scripts/addNewResource.js
@@ -1,3 +1,12 @@
+//appends an "active" class when the ADD button is clicked
+$(".open").on("click", function() {
+  $(".new-resource-modal, .new-resource-input").addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".cancel, .new-resource-modal").on("click", function() {
+  $(".new-resource-modal, .new-resource-input").removeClass("active");
+});
 // FUNCTIONS //
 
 // Create New Resource Element//
